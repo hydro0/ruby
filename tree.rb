479fec96b80be86cf31b87b7e1249ebb6f1cf7ed
Orest Savchak <osavchak@softjourn.com>
2016-04-29 11:29:28 +0300

    Fix tabs.

--- tree.rb
+++ tree.rb
@@ -2,11 +2,11 @@ require 'pp'
 
 Node = Struct::new(:text, :level, :children) do
   def to_s
-  	to_hash.to_s
+    to_hash.to_s
   end
 
   def to_hash
-  	{ text: text, level: level, children: children.map(&:to_hash) }
+    { text: text, level: level, children: children.map(&:to_hash) }
   end
 end
 
@@ -18,24 +18,24 @@ module NodesProvider
   ONLY_LEVEL_INPUT = 3
 
   def get_nodes(method)
-	puts "Enter number of nodes"
-	max_count = gets.to_i
+  puts "Enter number of nodes"
+  max_count = gets.to_i
 
-	if method == KEYBOARD_INPUT
-	  get_nodes_using_block(max_count) {
-	    Node::new( get_text_from_input, get_level_from_input, [] )
-	  }
-	elsif method == RANDOM_INIT
-	  get_nodes_using_block(max_count) {
-	    Node::new( get_random_text, 1 + rand(5), [] )
-	  }
-	elsif method == ONLY_LEVEL_INPUT
-	  get_nodes_using_block(max_count) {
-	    Node::new( get_random_text, get_level_from_input, [] )
-	  }
-	else
-	  raise "Method is not supported"
-	end
+  if method == KEYBOARD_INPUT
+    get_nodes_using_block(max_count) {
+      Node::new( get_text_from_input, get_level_from_input, [] )
+    }
+  elsif method == RANDOM_INIT
+    get_nodes_using_block(max_count) {
+      Node::new( get_random_text, 1 + rand(5), [] )
+    }
+  elsif method == ONLY_LEVEL_INPUT
+    get_nodes_using_block(max_count) {
+      Node::new( get_random_text, get_level_from_input, [] )
+    }
+  else
+    raise "Method is not supported"
+  end
   end
 
   def get_nodes_using_block(max_count)
@@ -48,17 +48,17 @@ module NodesProvider
   end
 
   def get_level_from_input
-  	puts "enter level:"
-  	gets.to_i
+    puts "enter level:"
+    gets.to_i
   end
 
   def get_text_from_input
-  	puts "enter text:"
-  	gets.chomp
+    puts "enter text:"
+    gets.chomp
   end
 
   def get_random_text
-  	(0...8).map { (65 + rand(26)).chr }.join
+    (0...8).map { (65 + rand(26)).chr }.join
   end
 
 end
@@ -67,19 +67,19 @@ module TreeBuilder
   module_function
 
   def get_tree(nodes)
-  	root = Node::new("Root", 0, [])
+    root = Node::new("Root", 0, [])
     parents = [root]
-  	while nodes.any?
-  	  node = nodes.shift
-  	  parent = parents.at(node.level - 1)
-  	  if parent
-  	    parent.children.push(node)
-  	    parents[node.level] = node
-  	  else
-  	  	puts "You have incorrect structure of your input data, no parent for #{node}"
-  	  end
-  	end
-  	root
+    while nodes.any?
+      node = nodes.shift
+      parent = parents.at(node.level - 1)
+      if parent
+        parent.children.push(node)
+        parents[node.level] = node
+      else
+        puts "You have incorrect structure of your input data, no parent for #{node}"
+      end
+    end
+    root
   end
 
 end
